bd7fbe7067308389122b74963eed59018254da7b
dgc1980 <dave@dgc1980.com>
2020-05-02 05:42:10 +0000

    changed report from users comment, to bots comment due to user deleting their comment

--- msg_monitor.py
+++ msg_monitor.py
@@ -89,8 +89,8 @@ while True:
                               msg.submission.mod.spoiler()
                               msg.submission.mod.flair(text='Expired', css_class='expired')
                               logging.info("flairing... responded to: " + msg.author.name)
-                              msg.reply("This deal has been marked expired as requested by "+msg.author.name+"  \nif this is a mistake reply with `"+Config.restore_trigger+"`").mod.distinguish(how='yes')
-                              msg.report('expiry request')
+                              myreply = msg.reply("This deal has been marked expired as requested by /u/"+msg.author.name+"  \nif this is a mistake reply with `"+Config.restore_trigger+"`").mod.distinguish(how='yes')
+                              myreply.report('expiry request')
                             else:
                               msg.report('Request expiration by new user')
                             msg.mark_read()
